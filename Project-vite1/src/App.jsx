db07f89e97f000cb268a60df99cd140dc50ce6d1
zhafran <zhafran1108@student.abudzar.sch.id>
2026-08-12 10:31:34 +0700

    p

--- Project-vite1/src/App.jsx
+++ Project-vite1/src/App.jsx
@@ -1,19 +1,42 @@
 /** @format */
 
 import Navbar from "./components/Navbar";
+import Profil from "./components/Profil";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
 import { Detail, Footer } from "./components/Footer";
+
+const profiles = [
+  {
+    name: "Altaf",
+    job: "Santri",
+  },
+  {
+    name: "Zhafran",
+    job: "Frontend Developer",
+  },
+  {
+    name: "Fawwaz",
+    job: "UI Designer",
+  },
+];
 
 // App adalah komponen utama
 export default function App() {
   return (
     <div className='min-h-screen bg-gray-100'>
       <Navbar />
+
+      {profiles.map((profile, index) => (
+        <Profil key={index} name={profile.name} job={profile.job} />
+      ))}
+
       <Hero />
+
       <main className='py-8'>
         <Card />
       </main>
+
       <Detail />
       <Footer />
     </div>
---
+++ Project-vite1/src/components/Profil.jsx
@@ -0,0 +1,11 @@
+/** @format */
+function Profil({ name, job }) {
+  return (
+    <>
+      <h1>Nama saya: {name}</h1>
+      <h2>Peran saya: {job}</h2>
+    </>
+  );
+}
+
+export default Profil;
